66b505ed4294e44e8278544c32901d12bf7ed2f7
zhenyu <huangzhenyu65@gmail.com>
2025-12-28 21:38:59 +0800

    fix lint

--- app/core/config.py
+++ app/core/config.py
@@ -11,9 +11,9 @@ VERSION: str = os.getenv("VERSION", "1.0.0")
 DEBUG: bool = os.getenv("DEBUG", "true").lower() == "true"
 
 # Database
-POSTGRES_USER: str = os.getenv("POSTGRES_USER", "user")
-POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD", "password")
-POSTGRES_DB: str = os.getenv("POSTGRES_DB", "database_name")
+POSTGRES_USER: str = os.getenv("POSTGRES_USER", "root")
+POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD", "123456")
+POSTGRES_DB: str = os.getenv("POSTGRES_DB", "game_member_hub")
 
 # 如果环境变量未设置，提供一个默认值或抛出错误
 DATABASE_URL: str = (
